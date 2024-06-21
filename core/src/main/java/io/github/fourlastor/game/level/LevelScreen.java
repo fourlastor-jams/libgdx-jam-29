b2f8f5d69b22bc4a3acec6b7406f08dfd8370131
Sandra Moen <sandramoen01@gmail.com>
2024-06-21 14:40:11 +0200

    screen fix + fun

--- core/src/main/java/io/github/fourlastor/game/level/LevelScreen.java
+++ core/src/main/java/io/github/fourlastor/game/level/LevelScreen.java
@@ -68,7 +68,7 @@ public class LevelScreen extends ScreenAdapter {
         this.atlas = atlas;
         this.random = random;
         music = assetManager.get(AssetsModule.PATH_MUSIC);
-        music.setVolume(0f);
+        music.setVolume(0.1f);
         music.setLooping(true);
         container = new StateContainer(State.initial());
         BitmapFont font = new BitmapFont(Gdx.files.internal("fonts/quan-pixel-8.fnt"));
