aded49b22bee7ee88a020fd2c2a04e4c4a51e1f1
Daniele Conti <fourlastor@users.noreply.github.com>
2024-06-22 17:37:52 +0200

    Show end game message

--- core/src/main/java/io/github/fourlastor/game/end/EndScreen.java
+++ core/src/main/java/io/github/fourlastor/game/end/EndScreen.java
@@ -1,38 +1,42 @@
 package io.github.fourlastor.game.end;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.InputMultiplexer;
-import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.ScreenAdapter;
-import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import io.github.fourlastor.game.route.Router;
 import javax.inject.Inject;
 
 public class EndScreen extends ScreenAdapter {
 
     public static Color CLEAR_COLOR = new Color(0x333333ff);
 
-    private final InputMultiplexer inputMultiplexer;
+    private final Router router;
     private final Stage stage;
     private final Viewport viewport;
+    private final BitmapFont font;
 
     @Inject
-    public EndScreen(InputMultiplexer inputMultiplexer, AssetManager assetManager, EndState endState) {
-        this.inputMultiplexer = inputMultiplexer;
-
+    public EndScreen(EndState endState, Router router) {
+        this.router = router;
         Gdx.app.log("EndState", endState.name());
         viewport = new FitViewport(256, 144);
         stage = new Stage(viewport);
-    }
-
-    @Override
-    public void show() {
-        inputMultiplexer.addProcessor(processor);
+        font = new BitmapFont(Gdx.files.internal("fonts/quan-pixel-16.fnt"));
+        Label.LabelStyle style = new Label.LabelStyle();
+        style.font = font;
+        String endMessage = endState == EndState.WON ? "You won" : "You lost";
+        Label wonLost = new Label(endMessage + "\nPress R to restart", style);
+        wonLost.setAlignment(Align.center);
+        wonLost.setPosition(stage.getWidth() / 2f, stage.getHeight() / 2f, Align.center);
+        stage.addActor(wonLost);
     }
 
     @Override
@@ -41,26 +45,19 @@ public class EndScreen extends ScreenAdapter {
     }
 
     @Override
-    public void hide() {
-        inputMultiplexer.removeProcessor(processor);
-    }
-
-    private final InputProcessor processor = new InputAdapter() {
-        @Override
-        public boolean keyUp(int keycode) {
-            return false;
-        }
-
-        @Override
-        public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-            return true;
-        }
-    };
-
-    @Override
     public void render(float delta) {
         ScreenUtils.clear(CLEAR_COLOR, true);
         stage.act(delta);
         stage.draw();
+        if (Gdx.input.isKeyJustPressed(Input.Keys.R)) {
+            router.goToLevel();
+        }
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        stage.dispose();
+        font.dispose();
     }
 }
